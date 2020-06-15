425384ab095eb1ac5fd8b1bd4bee752a2fcbf6c1
Mario <mario082@gmail.com>
2020-06-15 19:24:27 -0300

    switch - meses

--- basics.js
+++ basics.js
@@ -2,9 +2,51 @@
 
 let agora = new Date()
 let hora = agora.getHours()
+let minutos = agora.getMinutes()
 let dia = agora.getDate()
 let mes = agora.getMonth()
-console.log(`Agora são exatamente ${hora} horas, do dia ${dia} do ${mes + 1}.`) // problemas com o mês. (inicia o mẽs em 0)
+
+switch(mes) {
+    case 0:
+        mes = 'janeiro'
+        break
+    case 1:
+        mes = 'fevereiro'
+        break
+    case 2:
+        mes = 'março'
+        break
+    case 3:
+        mes = 'abril'
+        break
+    case 4:
+        mes = 'maio'
+        break
+    case 5:
+        mes = 'junho'
+        break
+    case 6:
+        mes = 'julho'
+        break
+    case 7:
+        mes = 'agosto'
+        break
+    case 8:
+        mes = 'setembro'
+        break
+    case 9:
+        mes = 'outubro'
+        break
+    case 10:
+        mes = 'novembro'
+        break
+    case 11:
+        mes = 'dezembro'
+        break
+
+}
+
+console.log(`Agora são exatamente ${hora}:${minutos}h, do dia ${dia} de ${mes}.`) // problemas com o mês. (inicia o mẽs em 0)
 
 if (hora < 12) {
     console.log('Bom Dia!')
@@ -42,3 +84,6 @@ switch(diaSem) {
         console.log('sábado')
     
 }
+
+
+
