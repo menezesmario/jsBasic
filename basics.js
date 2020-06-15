a5cb0de17013caad173f5d0ee349e1ea264c0a3d
Mario <mario082@gmail.com>
2020-06-14 22:09:35 -0300

    switch &  break

--- basics.js
+++ basics.js
@@ -1,10 +1,10 @@
-//cumprimentos
+//cumprimentos - Date - gethours
 
 let agora = new Date()
 let hora = agora.getHours()
 let dia = agora.getDate()
 let mes = agora.getMonth()
-console.log(`Agora são exatamente ${hora} horas, do dia ${dia} do ${mes + 1}.`) // problemas com o mês.
+console.log(`Agora são exatamente ${hora} horas, do dia ${dia} do ${mes + 1}.`) // problemas com o mês. (inicia o mẽs em 0)
 
 if (hora < 12) {
     console.log('Bom Dia!')
@@ -12,4 +12,33 @@ if (hora < 12) {
     console.log('Boa Tarde!')
 } else {
     console.log('Boa Noite!')
+}
+
+// switch - condição múltipla
+
+let agora2 = new Date()
+let diaSem = agora.getDay()
+
+switch(diaSem) {
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2: 
+        console.log('terça')
+        break
+    case 3:
+        console.log('quarta')
+        break
+    case 4:
+        console.log('quinta')
+        break
+    case 5:
+        console.log('sexta')
+        break
+    case 6:
+        console.log('sábado')
+    
 }
